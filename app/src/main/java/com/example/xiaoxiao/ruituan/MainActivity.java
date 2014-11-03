535eec2580b9ee17715cce6776c6ffdc93a37fa6
liushengbin512 <liushengbin@zrworldwide.com>
2014-11-03 14:37:48 +0800

    添加头部标题

--- app/src/main/java/com/example/xiaoxiao/ruituan/MainActivity.java
+++ app/src/main/java/com/example/xiaoxiao/ruituan/MainActivity.java
@@ -3,15 +3,17 @@ package com.example.xiaoxiao.ruituan;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTabHost;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -50,7 +52,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         mDrawer_layout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mMenu_layout = (LinearLayout) findViewById(R.id.menu_layout);
         ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_layout, TabOneFragment.newInstance(null,null));
+        ft.replace(R.id.fragment_layout, TabOneFragment.newInstance(null, null));
         ft.addToBackStack(null);
         ft.commit();
         tv_tab1= (TextView) findViewById(R.id.tv_tab1);
@@ -121,4 +123,35 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         ft.commit();
         mDrawer_layout.closeDrawer(mMenu_layout);//点击后关闭mMenu_layout
     }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            // 创建退出对话框
+            AlertDialog.Builder isExit= new AlertDialog.Builder(this);
+            // 设置对话框标题
+            isExit.setTitle("系统提示");
+            // 设置对话框消息
+            isExit.setMessage("确定要退出吗");
+            // 添加选择按钮并注册监听
+            isExit.setPositiveButton("确定", listener);
+            isExit.setNegativeButton("取消", listener);
+            // 显示对话框
+            isExit.show();
+        }
+        return false;
+    }
+    /**监听对话框里面的button点击事件*/
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case AlertDialog.BUTTON_POSITIVE:// "确认"按钮退出程序
+                    finish();
+                    break;
+                case AlertDialog.BUTTON_NEGATIVE:// "取消"第二个按钮取消对话框
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
